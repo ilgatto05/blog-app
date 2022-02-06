01acbc2960727488199f25560d1dad0471a70c25
Alan <alan.g05@hotmail.com>
2022-02-06 19:35:03 -0300

    comment added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,30 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to @article
     else
-      render :new
+      flash.now[:notice]="not able to create"
+      render :new , status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
 
   private
